b239ade064893c679388c4079ae9a11eaf3e0485
Diana <diapa22@gmail.com>
2025-02-25 17:43:30 -0500

    add interactive slider

--- script.js
+++ script.js
@@ -12,3 +12,36 @@ menu.addEventListener('click',() =>{
         menu.classList.remove('fa-times');
     }
 })
+
+// slaider interactivo
+const totalImages = document.querySelectorAll('.gallery-item');
+const galleryContainer = document.querySelector('.gallery-container');
+
+let currentIndex = 0; // mantiene un registro de la imagen actual que se esta mostrando en el slider
+
+// funcion para actualizar la posición del contenedor
+function updateGalleryPosition(){
+    galleryContainer.style.transform = `translateX(-${currentIndex * 25}%)`;
+   }
+   
+document.querySelector('.prev-button').addEventListener('click',()=>{
+   currentIndex--;
+
+   if(currentIndex<0){
+    currentIndex = totalImages.length -1 // regresa al final del carrusel
+   }
+   updateGalleryPosition()
+});
+
+
+document.querySelector('.next-button').addEventListener('click',()=>{
+    currentIndex++;
+
+   if(currentIndex >= totalImages.length){
+    currentIndex = 0; // regresa al principio del carrusel
+   }
+
+   updateGalleryPosition()
+})
+
+
